76744e3abcb3dd2b3661a37995f15f445dd1ab8c
aurielcp <113363249+aurielcp@users.noreply.github.com>
2023-02-23 22:08:48 -0600

    Tarea 3

---
+++ JuegoGatoMarco/src/juegogatomarco/JuegoGatoMarco.java
@@ -0,0 +1,22 @@
+public class JuegoGatoMarco {
+
+    public static void main(String[] args) {
+        char columnas = 124;
+        char filas = 45;
+        System.out.print(filas);
+        System.out.print(filas);
+        System.out.print(filas);
+        System.out.print(filas);
+        System.out.println(filas);
+        System.out.println(columnas + " " + " " + " " + columnas);
+        System.out.println(columnas + " " + " " + " " + columnas);
+        System.out.println(columnas + " " + " " + " " + columnas);
+        System.out.print(filas);
+        System.out.print(filas);
+        System.out.print(filas);
+        System.out.print(filas);
+        System.out.println(filas);        
+
+    }
+    
+}
