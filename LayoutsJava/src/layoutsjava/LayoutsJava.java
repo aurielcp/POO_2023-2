99a0c304bb3fa84589ba3804ac2841d71628dd9d
aurielcp <113363249+aurielcp@users.noreply.github.com>
2023-05-07 20:42:41 -0600

    Tarea 13

--- LayoutsJava/src/layoutsjava/LayoutsJava.java
+++ LayoutsJava/src/layoutsjava/LayoutsJava.java
@@ -2,7 +2,7 @@ package layoutsjava;
 public class LayoutsJava {
     public static void main(String[] args) {
         //Ventana ventana = new Ventana("Layouts");
-        VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layout cruadricula");
+        //VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layout cruadricula");
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Farenheit");
     }
-    
 }
